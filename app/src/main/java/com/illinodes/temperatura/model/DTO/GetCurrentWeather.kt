3a367a15ace1ad7cd92f2f18963cb25cb6d9fe83
Theto Mokganya <theto@onecart.co.za>
2023-07-15 14:33:28 +0200

    Added retrofit service

---
+++ app/src/main/java/com/illinodes/temperatura/model/DTO/GetCurrentWeather.kt
@@ -0,0 +1,5 @@
+package com.illinodes.temperatura.model.DTO
+
+data class GetCurrentWeather(
+    var abc: String,
+)
---
+++ app/src/main/java/com/illinodes/temperatura/model/DTO/GetWeatherForecast.kt
@@ -0,0 +1,5 @@
+package com.illinodes.temperatura.model.DTO
+
+data class GetWeatherForecast(
+    var abc: String,
+)
