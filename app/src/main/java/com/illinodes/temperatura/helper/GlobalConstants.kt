c116351e179bc97879adfb027896342244c49a26
Theto Mokganya <theto@onecart.co.za>
2023-07-17 09:38:11 +0200

    Removed api key

--- app/src/main/java/com/illinodes/temperatura/helper/GlobalConstants.kt
+++ app/src/main/java/com/illinodes/temperatura/helper/GlobalConstants.kt
@@ -2,5 +2,5 @@ package com.illinodes.temperatura.helper
 
 object GlobalConstants {
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-    const val API_KEY = "a49506a0bcb4d513ac9ada50299eaa0e"
+    const val API_KEY = "OPEN_WEATHER_API_KEY_HERE"
 }
